32e9d80fd6deb923c608526894ce1d258e52c7e7
Switch <tminraz@gmail.com>
2017-05-18 01:15:26 +0300

    1

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
-import { ping } from './enhancers/ping'
+import { createLogger } from 'redux-logger'
 
 export default function configureStore(initialState) {
+  const logger = createLogger()
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(ping))
+    applyMiddleware(logger))
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
